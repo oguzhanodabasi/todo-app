b70739dca576611b79123f856b5878e0b5728702
Oğuzhan Odabaşı <modaozi70@gmail.com>
2025-04-21 15:22:21 +0300

    frontend -> baseURL olarak render prod URL girildi.

--- todoApp-frontend/src/services/axios.js
+++ todoApp-frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = import.meta.env.PROD
-    ? 'https://my-backend.up.railway.app/api'
+    ? 'https://todo-app-backend-hc7y.onrender.com/api'
     : 'http://localhost:8001/api';
 
 const api = axios.create({
